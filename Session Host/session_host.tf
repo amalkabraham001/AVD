05ba15d89d5ed3ebfac92f45c583b38f1da80fca
amalkabraham001 <78976854+amalkabraham001@users.noreply.github.com>
2023-05-23 18:41:52 +0530

    Create session_host.tf

---
+++ Session Host/session_host.tf
@@ -0,0 +1,129 @@
+locals {
+  registration_token = azurerm_virtual_desktop_host_pool_registration_info.registrationinfo.token
+shutdown_command     = "shutdown -r -t 10"
+  exit_code_hack       = "exit 0"
+  commandtorun         = "New-Item -Path HKLM:/SOFTWARE/Microsoft/RDInfraAgent/AADJPrivate"
+  powershell_command   = "${local.commandtorun}; ${local.shutdown_command}; ${local.exit_code_hack}"
+}
+
+resource "random_string" "AVD_local_password" {
+  count            = var.rdsh_count
+  length           = 16
+  special          = true
+  min_special      = 2
+  override_special = "*!@#?"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "avd-session-rg"
+  location = var.resource_group_location
+}
+
+resource "azurerm_network_interface" "avd_vm_nic" {
+  count               = var.rdsh_count
+  name                = "${var.prefix}-${count.index + 1}-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "nic${count.index + 1}_config"
+    subnet_id                     = azurerm_subnet.spoke1-workload.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+resource "azurerm_windows_virtual_machine" "avd_vm" {
+  count                 = var.rdsh_count
+  name                  = "${var.prefix}-${count.index + 1}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  network_interface_ids = ["${azurerm_network_interface.avd_vm_nic.*.id[count.index]}"]
+  provision_vm_agent    = true
+  admin_username        = var.local_admin_username
+  admin_password        = var.local_admin_password
+
+  os_disk {
+    name                 = "${lower(var.prefix)}-${count.index + 1}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "win10-22h2-avd-g2"
+    version   = "latest"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  depends_on = [
+    azurerm_resource_group.rg,
+    azurerm_network_interface.avd_vm_nic
+  ]
+}
+
+
+resource "azurerm_virtual_machine_extension" "vmext_dsc" {
+  count                      = var.rdsh_count
+  name                       = "${var.prefix}${count.index + 1}-avd_dsc"
+  virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
+  publisher                  = "Microsoft.Powershell"
+  type                       = "DSC"
+  type_handler_version       = "2.73"
+  auto_upgrade_minor_version = true
+
+  settings = <<-SETTINGS
+    {
+      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_09-08-2022.zip",
+      "configurationFunction": "Configuration.ps1\\AddSessionHost",
+      "properties": {
+        "HostPoolName":"${azurerm_virtual_desktop_host_pool.hostpool.name}"
+      }
+    }
+SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+  {
+    "properties": {
+      "registrationInfoToken": "${local.registration_token}"
+    }
+  }
+PROTECTED_SETTINGS
+
+  depends_on = [
+
+    azurerm_virtual_desktop_host_pool.hostpool
+  ]
+}
+resource "azurerm_virtual_machine_extension" "domain_join" {
+  count                      = var.rdsh_count
+  name                       = "${var.prefix}-${count.index + 1}-domainJoin"
+  virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
+  publisher                  = "Microsoft.Azure.ActiveDirectory"
+  type                       = "AADLoginForWindows"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+}
+resource "azurerm_virtual_machine_extension" "AADJPRIVATE" {
+    depends_on = [
+      azurerm_virtual_machine_extension.domain_join
+    ]
+    count = 2
+  name                 = "AADJPRIVATE"
+  virtual_machine_id =   azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
+    }
+SETTINGS
+}
